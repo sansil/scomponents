ffa01dae5182aea415d55a8dcff0fbc36f87a3c6
sansil <sansil32@gmail.com>
2020-09-04 14:32:27 -0300

    add some test

--- src/components/SButton.vue
+++ src/components/SButton.vue
@@ -14,6 +14,7 @@
 
 <script>
 export default {
+  name: "SButton",
   props: {
     kind: {
       type: String,
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,52 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import SButton from '@/components/SButton.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+
+describe('SButton.vue', () => {
+  // beforeAll(() => {
+  //   wrapperSlotted = mount(SButton, {
+  //     // data () {
+  //     //   return { myProp: 'small' }
+  //     // },
+  //     propsData: {
+  //       as: 'a',
+  //       href: '#'
+  //     },
+  //     slot: {
+  //       default: `<div>sansil</div>`,
+  //     },
+  //   });
+  // });
+
+  // it('render slot', () => {
+  //   //let text = wrapperSlotted.find('div')
+  //   //expect(wrapperSlotted.is('span')).toBe(true)
+  //   expect(wrapperSlotted.html()).toContain('<div>sansil</div>')
+  // });
+
+  it('render as anchor', () => {
+    // render the component
+    const wrapper = shallowMount(SButton, {
+      propsData: {
+        as: 'a',
+        href: '#'
+      }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.findAll('a').length).toEqual(1)
+    wrapper.find('a').trigger('click')
+    //const clickCall = wrapper.emitted('click')
+    // expect(clickCall).toHaveLength(1)
+    expect(wrapper.find('a').attributes().href).toBe('#')
   })
-})
+  it('render as button', () => {
+    // render the component
+    const wrapper = shallowMount(SButton, {
+      propsData: {
+        as: 'button',
+        type: 'button'
+      }
+    })
+    expect(wrapper.findAll('button').length).toEqual(1)
+    expect(wrapper.find('button').attributes().type).toBe('button')
+  })
+});
